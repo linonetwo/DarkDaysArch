58f58db303b674873c4e9c455b985d855e25ba1f
Lin Onetwo <linonetwo012@gmail.com>
2021-08-15 12:50:52 +0800

    feat: install more react deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-export default function App() {
+export default function App(): JSX.Element {
+  const { t } = useTranslation();
   return (
     <div className="App">
-      <header className="App-header">Hi</header>
+      <header className="App-header">Hi {t('LinOnetwo')}</header>
     </div>
   );
 }
