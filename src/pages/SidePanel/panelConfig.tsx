c5852836a78c90c00d9bb15e3c307183c93e70ea
林一二 <linonetwo012@gmail.com>
2021-08-15 18:50:56 +0800

    feat: add more panels

--- src/pages/SidePanel/panelConfig.tsx
+++ src/pages/SidePanel/panelConfig.tsx
@@ -9,12 +9,52 @@ export interface IPanelConfig {
 export const panelConfig: IPanelConfig[] = [
   {
     button: {
+      icon: 'InsertDriveFile',
+      id: 'File',
+    },
+    controls: [
+      { icon: 'List', id: 'OpenedFiles', component: <div>aaa</div> },
+      { icon: 'AccountTree', id: 'FileTree', component: <div>bbb</div> },
+    ],
+  },
+  {
+    button: {
       icon: 'Add',
       id: 'Item',
     },
     controls: [
       { icon: 'History', id: 'ItemHistory', component: <div>aaa</div> },
       { icon: 'EmojiSymbols', id: 'ItemList', component: <div>bbb</div> },
+    ],
+  },
+  {
+    button: {
+      icon: 'Streetview',
+      id: 'Layer',
+    },
+    controls: [
+      { icon: 'NaturePeople', id: 'Layers', component: <div>aaa</div> },
+      { icon: 'Accessible', id: 'LayerHelp', component: <div>bbb</div> },
+    ],
+  },
+  {
+    button: {
+      icon: 'Search',
+      id: 'Search',
+    },
+    controls: [
+      { icon: 'Search', id: 'Search', component: <div>aaa</div> },
+      { icon: 'Help', id: 'SearchHelp', component: <div>bbb</div> },
+    ],
+  },
+  {
+    button: {
+      icon: 'Games',
+      id: 'Simulation',
+    },
+    controls: [
+      { icon: 'Search', id: 'Search', component: <div>aaa</div> },
+      { icon: 'Help', id: 'SearchHelp', component: <div>bbb</div> },
     ],
   },
 ];
