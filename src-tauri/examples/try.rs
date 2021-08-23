2becbcef10a65aba7ecdcf2368c16d298d0fd679
林一二 <linonetwo012@gmail.com>
2021-08-24 00:51:13 +0800

    Update try.rs

--- src-tauri/examples/try.rs
+++ src-tauri/examples/try.rs
@@ -1,15 +1,15 @@
+use serde_json;
 use std::fs::File;
 use std::io::Read;
-use serde_json;
-#[path = "../src/types/mapgen.rs"]
+#[path = "../src/types/palette.rs"]
 mod mapgen_json;
 
 fn main() {
-  let map_file_path = "/Users/linonetwo/Desktop/repo/DarkDaysArch/src/store/models/magic_academy.json";
+  let map_file_path = "/Users/lindongwu/Desktop/github/DarkDaysArch/src/store/models/house_general_palette.json";
   let mut raw_map_file = File::open(map_file_path).unwrap();
   let mut raw_map_string = String::new();
   raw_map_file.read_to_string(&mut raw_map_string).unwrap();
-  let raw_map: mapgen_json::CDDAMapgenArray = serde_json::from_str(&raw_map_string).unwrap();
+  let raw_map: mapgen_json::CDDAPaletteArray = serde_json::from_str(&raw_map_string).unwrap();
 
   println!("{:?}", raw_map);
 }
