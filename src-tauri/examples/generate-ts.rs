d0642d95659b410ec2aa4cb1962e0509155332c2
Jinser <cmdr.jv@gmail.com>
2021-08-25 02:07:58 +0800

    Improve structure

--- src-tauri/examples/generate-ts.rs
+++ src-tauri/examples/generate-ts.rs
@@ -1,7 +1,9 @@
 use project_root::get_project_root;
-use std::fs::write;
-use std::path::Path;
-use typescript_type_def::write_definition_file;
+use std::{
+  fs::write,
+  path::{Path, PathBuf},
+};
+use typescript_type_def::{write_definition_file, TypeDef};
 
 #[path = "../src/types/furniture.rs"]
 mod furniture_json;
@@ -10,29 +12,19 @@ mod mapgen_json;
 #[path = "../src/types/palette.rs"]
 mod palette_json;
 
+fn generate<T>(path: PathBuf)
+where
+  T: ?Sized + TypeDef,
+{
+  let mut buf = Vec::new();
+  write_definition_file::<_, T>(&mut buf, Default::default()).unwrap();
+  let generated_ts_content = String::from_utf8(buf).unwrap();
+  write(path, generated_ts_content).expect("can not write ts file");
+}
+
 fn main() {
-    // let mut buf = Vec::new();
-    // write_definition_file::<_, mapgen_json::CDDAMapgenWithCache>(&mut buf, Default::default()).unwrap();
-    // let generated_ts_content = String::from_utf8(buf).unwrap();
-    // let ts_file_path = Path::join(&get_project_root().unwrap(), "../src/types/mapgen.ts");
-    // // println!("{}", ts_file_path.into_os_string().into_string().unwrap());
-    // write(ts_file_path, generated_ts_content).expect("can not write ts file");
-
-    // let mut buf = Vec::new();
-    // write_definition_file::<_, palette_json::CDDAPaletteArray>(&mut buf, Default::default()).unwrap();
-    // let generated_ts_content = String::from_utf8(buf).unwrap();
-    // let ts_file_path = Path::join(&get_project_root().unwrap(), "../src/types/palette.ts");
-    // // println!("{}", ts_file_path.into_os_string().into_string().unwrap());
-    // write(ts_file_path, generated_ts_content).expect("can not write ts file");
-
-    let mut buf = Vec::new();
-    write_definition_file::<_, furniture_json::CDDAFurnArray>(&mut buf, Default::default())
-        .unwrap();
-    let generated_ts_content = String::from_utf8(buf).unwrap();
-    let ts_file_path = Path::join(
-        &get_project_root().unwrap(),
-        "../src/types/cdda/furniture.ts",
-    );
-    // println!("{}", ts_file_path.into_os_string().into_string().unwrap());
-    write(ts_file_path, generated_ts_content).expect("can not write ts file");
+  let project_root = &get_project_root().unwrap();
+  generate::<mapgen_json::CDDAMapgenWithCache>(Path::join(project_root, "../src/types/mapgen.ts"));
+  generate::<palette_json::CDDAPaletteArray>(Path::join(project_root, "../src/types/palette.ts"));
+  generate::<furniture_json::CDDAFurnArray>(Path::join(project_root, "../src/types/furniture.ts"));
 }
