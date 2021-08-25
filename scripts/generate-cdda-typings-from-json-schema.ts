869103e1e8f7457ebbf8c36f6b6ba1709418f205
林一二 <linonetwo012@gmail.com>
2021-08-25 23:35:58 +0800

    feat: generate TS from JSONSchema generated from Rust serde

---
+++ scripts/generate-cdda-typings-from-json-schema.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+import { compileFromFile } from 'json-schema-to-typescript';
+
+const cddaTypingsFolder = path.join(__dirname, '..', 'src', 'types', 'cdda');
+const fileNames = fs.readdirSync(cddaTypingsFolder);
+const jsonSchemaFilePaths = fileNames.filter((fileName) => fileName.endsWith('.json')).map((fileName) => path.join(cddaTypingsFolder, fileName));
+
+void jsonSchemaFilePaths.map(
+  async (fileName) => await compileFromFile(fileName).then((tsContent) => fs.writeFileSync(fileName.replace('.json', '.ts'), tsContent)),
+);
--- src-tauri/examples/generate-ts.rs
+++ src-tauri/examples/generate-ts.rs
@@ -3,8 +3,16 @@ use std::{
   fs::write,
   path::{Path, PathBuf},
 };
-use typescript_type_def::{write_definition_file, TypeDef};
+use schemars::{schema_for, JsonSchema};
 
+/**
+ * convert serde to jsonschema: https://imfeld.dev/writing/generating_typescript_types_from_rust
+ *  with way to optimize
+ * convert jsonschema to ts: https://github.com/bcherny/json-schema-to-typescript
+*/
+
+#[path = "../src/types/tileset.rs"]
+mod tileset_json;
 #[path = "../src/types/furniture.rs"]
 mod furniture_json;
 #[path = "../src/types/mapgen.rs"]
@@ -14,17 +22,17 @@ mod palette_json;
 
 fn generate<T>(path: PathBuf)
 where
-  T: ?Sized + TypeDef, // Sized or ?Sized are both ok, click https://zhuanlan.zhihu.com/p/21820917 to learn why
+  T: ?Sized + JsonSchema, // Sized or ?Sized are both ok, click https://zhuanlan.zhihu.com/p/21820917 to learn why
 {
-  let mut buf = Vec::new();
-  write_definition_file::<_, T>(&mut buf, Default::default()).unwrap();
-  let generated_ts_content = String::from_utf8(buf).unwrap();
-  write(path, generated_ts_content).expect("can not write ts file");
+  let schema = schema_for!(T);
+  let output = serde_json::to_string_pretty(&schema).unwrap();
+  write(path, output).expect("can not write json-schema file")
 }
 
 fn main() {
   let project_root = &get_project_root().unwrap();
-  generate::<mapgen_json::CDDAMapgenWithCache>(Path::join(project_root, "../src/types/cdda/mapgen.ts"));
-  generate::<palette_json::CDDAPaletteArray>(Path::join(project_root, "../src/types/cdda/palette.ts"));
-  generate::<furniture_json::CDDAFurnArray>(Path::join(project_root, "../src/types/cdda/furniture.ts"));
+  generate::<tileset_json::CDDATileSetConfigWithCache>(Path::join(project_root, "../src/types/cdda/tileset.json"));
+  generate::<mapgen_json::CDDAMapgenWithCache>(Path::join(project_root, "../src/types/cdda/mapgen.json"));
+  generate::<palette_json::CDDAPaletteArray>(Path::join(project_root, "../src/types/cdda/palette.json"));
+  generate::<furniture_json::CDDAFurnArray>(Path::join(project_root, "../src/types/cdda/furniture.json"));
 }
