73a5c3e974ff9cd260a6c3be5e7bc13383e44e04
林一二 <linonetwo012@gmail.com>
2021-08-26 23:15:02 +0800

    feat: support randomly picked tiles

--- src-tauri/examples/try.rs
+++ src-tauri/examples/try.rs
@@ -14,25 +14,25 @@ fn main() {
   // println!("{:?}", raw_map);
   println!("{}",map_json);
 
-  // let palette_file_path = "../public/json/house_general_palette.json";
-  // let mut raw_palette_file = File::open(palette_file_path).unwrap();
-  // let mut raw_palette_string = String::new();
-  // raw_palette_file.read_to_string(&mut raw_palette_string).unwrap();
-  // let raw_palette: palette::CDDAPaletteArray = serde_json::from_str(&raw_palette_string).unwrap();
+  let palette_file_path = "../public/json/house_general_palette.json";
+  let mut raw_palette_file = File::open(palette_file_path).unwrap();
+  let mut raw_palette_string = String::new();
+  raw_palette_file.read_to_string(&mut raw_palette_string).unwrap();
+  let raw_palette: palette::CDDAPaletteArray = serde_json::from_str(&raw_palette_string).unwrap();
 
-  // let palette_json = serde_json::to_string(&raw_palette).unwrap();
+  let palette_json = serde_json::to_string(&raw_palette).unwrap();
 
-  // // println!("{:?}", raw_palette);
-  // println!("{}", palette_json);
+  // println!("{:?}", raw_palette);
+  println!("{}", palette_json);
 
-  // let furn_file_path = "../public/json/furniture-medical.json";
-  // let mut raw_furn_file = File::open(furn_file_path).unwrap();
-  // let mut raw_furn_string = String::new();
-  // raw_furn_file.read_to_string(&mut raw_furn_string).unwrap();
-  // let raw_furn: furniture::CDDAFurnArray = serde_json::from_str(&raw_furn_string).unwrap();
+  let furn_file_path = "../public/json/furniture-medical.json";
+  let mut raw_furn_file = File::open(furn_file_path).unwrap();
+  let mut raw_furn_string = String::new();
+  raw_furn_file.read_to_string(&mut raw_furn_string).unwrap();
+  let raw_furn: furniture::CDDAFurnArray = serde_json::from_str(&raw_furn_string).unwrap();
 
-  // let furn_json = serde_json::to_string(&raw_furn).unwrap();
+  let furn_json = serde_json::to_string(&raw_furn).unwrap();
 
-  // println!("{:?}", raw_furn);
-  // println!("{}",furn_json);
+  println!("{:?}", raw_furn);
+  println!("{}",furn_json);
 }
