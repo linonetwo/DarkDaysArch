9c906f6b966e25aef138028d82467b3ba16ea7c7
Lin Onetwo <linonetwo012@gmail.com>
2021-08-29 00:02:15 +0800

    feat: add derived field sprite_width_ratio

--- src-tauri/src/parsers/tileset.rs
+++ src-tauri/src/parsers/tileset.rs
@@ -1,31 +1,27 @@
-use std::{collections::BTreeMap, fs::File, io::Read};
 use regex::Regex;
+use std::{collections::BTreeMap, fs::File, io::Read};
 
-use data::types::{tileset};
+use data::types::tileset;
 
 pub fn prepare_tile_data_index(raw_tile_config: tileset::CDDATileSetConfig) -> BTreeMap<String, tileset::CDDATileSetInverseIndexedTileData> {
   let id_start_matching_regex = Regex::new(r"range (\d+) to (\d+)").unwrap();
   let mut tile_data_index: BTreeMap<String, tileset::CDDATileSetInverseIndexedTileData> = BTreeMap::new();
-    // prepare inverse index
-    let default_tileset_info = tileset::CDDATileSetTileInfo {
-      width: 32,
-      height: 32,
-      pixelscale: 1,
-    };
+  // prepare inverse index
+  let default_tileset_info = tileset::CDDATileSetTileInfo {
+    width: 32,
+    height: 32,
+    pixelscale: 1,
+  };
+  let tileset_info = raw_tile_config.tile_info.get(0).unwrap_or(&default_tileset_info);
   for tile_config_item in &raw_tile_config.tiles_new {
     let mut cloned_tile_config_item = tile_config_item.clone();
     cloned_tile_config_item.tiles = vec![];
     cloned_tile_config_item.ascii = vec![];
-    cloned_tile_config_item.sprite_height = Some(
-      cloned_tile_config_item
-        .sprite_height
-        .unwrap_or(raw_tile_config.tile_info.get(0).unwrap_or(&default_tileset_info).height),
-    );
-    cloned_tile_config_item.sprite_width = Some(
-      cloned_tile_config_item
-        .sprite_width
-        .unwrap_or(raw_tile_config.tile_info.get(0).unwrap_or(&default_tileset_info).width),
-    );
+    cloned_tile_config_item.sprite_height = Some(cloned_tile_config_item.sprite_height.unwrap_or(tileset_info.height));
+    cloned_tile_config_item.sprite_width = Some(cloned_tile_config_item.sprite_width.unwrap_or(tileset_info.width));
+    // we store ratio of width height of tile to the tileset config width height, and we multiply this ratio to the visual width height defined in the render side
+    cloned_tile_config_item.sprite_height_ratio = Some(cloned_tile_config_item.sprite_height.unwrap() / tileset_info.height);
+    cloned_tile_config_item.sprite_width_ratio = Some(cloned_tile_config_item.sprite_width.unwrap() / tileset_info.width);
     cloned_tile_config_item.sprite_offset_x = Some(cloned_tile_config_item.sprite_offset_x.unwrap_or(0));
     cloned_tile_config_item.sprite_offset_y = Some(cloned_tile_config_item.sprite_offset_y.unwrap_or(0));
     cloned_tile_config_item.comment = Some(cloned_tile_config_item.comment.unwrap_or(String::from("range 1 to 63")));
