14ddb7b2de072bdc2842de4f4615c218db6df705
林一二 <linonetwo012@gmail.com>
2021-09-02 22:35:18 +0800

    fix: value in EXTERNAL_OPTION can be anything

--- src-tauri/data/src/types/external_option.rs
+++ src-tauri/data/src/types/external_option.rs
@@ -1,15 +1,16 @@
 use crate::common::string::{EXTERNAL_OPTION_Literal, ITEM_BLACKLIST_Literal};
 use schemars::JsonSchema;
 use serde::{self, Deserialize, Serialize};
+use serde_json::value;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema)]
 pub struct CDDAExternalOption {
   #[serde(rename = "type")]
   pub cdda_json_type: EXTERNAL_OPTION_Literal,
   pub name: String,
-  pub info: String,
+  pub info: Option<String>,
   pub stype: String,
-  pub value: f64,
+  pub value: value::Value,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, JsonSchema)]
